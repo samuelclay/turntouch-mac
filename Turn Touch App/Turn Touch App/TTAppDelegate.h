69a54d41f48b35da0adb96a4f475573b844524e6
Samuel Clay <samuel@ofbrooklyn.com>
2013-08-01 19:12:55 -0700

    Stubbing in mac app.

---
+++ Turn Touch App/Turn Touch App/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  Turn Touch App
+//
+//  Created by Samuel Clay on 8/1/13.
+//  Copyright (c) 2013 Turn Touch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TTAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Turn Touch App/Turn Touch App/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Turn Touch App
+//
+//  Created by Samuel Clay on 8/1/13.
+//  Copyright (c) 2013 Turn Touch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
