e84dfbdb1a87619a5e02692684cfef71e7a3d4ba
Samuel Clay <samuel@ofbrooklyn.com>
2015-12-20 16:20:28 -0800

    Stubbing in new batch action views.

---
+++ Turn Touch Remote/Views/Batch Actions/TTBatchActionContainerView.m
@@ -0,0 +1,19 @@
+//
+//  TTBatchActionContainerView.m
+//  Turn Touch Remote
+//
+//  Created by Samuel Clay on 12/20/15.
+//  Copyright © 2015 Turn Touch. All rights reserved.
+//
+
+#import "TTBatchActionContainerView.h"
+
+@implementation TTBatchActionContainerView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
---
+++ Turn Touch Remote/Views/Batch Actions/TTBatchActionHeaderView.m
@@ -0,0 +1,19 @@
+//
+//  TTBatchActionHeaderView.m
+//  Turn Touch Remote
+//
+//  Created by Samuel Clay on 12/20/15.
+//  Copyright © 2015 Turn Touch. All rights reserved.
+//
+
+#import "TTBatchActionHeaderView.h"
+
+@implementation TTBatchActionHeaderView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
